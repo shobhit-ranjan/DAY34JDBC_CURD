242202fca9a5c3db76b9cf24f05c08515ac51234
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-09-01 17:32:06 +0530

    UC2

--- Connection_JDBC.java
+++ Connection_JDBC.java
@@ -1,34 +1,67 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Connection_JDBC {
 
-	 public static void main(String[] args) {
-		 Connection conn = null;
-		 try {
-			String hostUrl = "jdbc:mysql://localhost:3306/payroll_services";
-			String userName = "root";
-			String password = "mymaths009!@A";
+	public static void main(String[] args) {
+		getsqlConnection();
+		readEmployeePayroll();
 
-			conn = DriverManager.getConnection(hostUrl, userName, password);
+	}
+	private static void readEmployeePayroll() {
+		System.out.println("Displaying all data of employee_payroll table");
+		Connection conn = getsqlConnection();
 
+		try {
 			if (conn != null) {
-				System.out.println("Connection is made.!");
+				String readEmpPayroll = "SELECT * FROM employee_payrollday34";
+
+				Statement statement = conn.createStatement();
+				ResultSet resultSet = statement.executeQuery(readEmpPayroll);
+				while (resultSet.next()) {
+					Integer id = resultSet.getInt(1);
+					String name = resultSet.getString(2);
+					Integer salary = resultSet.getInt(3);
+					String date = resultSet.getString(4);
+					String sal = resultSet.getString(4);
+					String row = String.format(
+							"User record: \n Id: %d, \n Name: %s,\n Salary: %d, \n Date: %s, \n Sal: %s ", id,
+							name, salary, date,sal);
+					System.out.println(row);
+				}
 			}
-		} catch (SQLException sqlException) {
-			System.out.println(sqlException.getMessage());
+		} catch (SQLException e) {
+			e.printStackTrace();
 		} finally {
 			if (conn != null) {
 				try {
 					conn.close();
-				} catch (SQLException e) {
-					System.out.println(e.getMessage());
+				} catch (SQLException sqlException) {
+					System.out.println(sqlException.getMessage());
+
 				}
 			}
 		}
 
 	}
+	private static Connection getsqlConnection() {
+		Connection conn = null;
+		String hostUrl = "jdbc:mysql://localhost:3306/payroll_services";
+		String userName = "root";
+		String password = "mymaths009!@A";
+		try {
+			conn = DriverManager.getConnection(hostUrl, userName, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+		return conn;
+
+	}
+
 
 }
 
