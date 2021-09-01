2f72c44d87e40b4c8dbf7ab31e5fe56b88bd922a
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-09-01 17:22:28 +0530

    first commit

---
+++ Connection_JDBC.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connection_JDBC {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		try {
+			String hostUrl = "jdbc:mysql://localhost:3306/payroll_services";
+			String userName = "root";
+			String password = "mymaths009!@A";
+
+			conn = DriverManager.getConnection(hostUrl, userName, password);
+
+			if (conn != null) {
+				System.out.println("Connection is made.!");
+			}
+		} catch (SQLException sqlException) {
+			System.out.println(sqlException.getMessage());
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+
+	}
+
+}
+
