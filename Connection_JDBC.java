15dea9dff4727fd49af6b494ef9aeb86eb4f87bb
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-09-01 17:47:44 +0530

    UC3 update

--- Connection_JDBC.java
+++ Connection_JDBC.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,9 +9,11 @@ public class Connection_JDBC {
 
 	public static void main(String[] args) {
 		getsqlConnection();
-		readEmployeePayroll();
+		// readEmployeePayroll();
+		writeempData();
 
 	}
+
 	private static void readEmployeePayroll() {
 		System.out.println("Displaying all data of employee_payroll table");
 		Connection conn = getsqlConnection();
@@ -28,8 +31,8 @@ public class Connection_JDBC {
 					String date = resultSet.getString(4);
 					String sal = resultSet.getString(4);
 					String row = String.format(
-							"User record: \n Id: %d, \n Name: %s,\n Salary: %d, \n Date: %s, \n Sal: %s ", id,
-							name, salary, date,sal);
+							"User record: \n Id: %d, \n Name: %s,\n Salary: %d, \n Date: %s, \n Sal: %s ", id, name,
+							salary, date, sal);
 					System.out.println(row);
 				}
 			}
@@ -46,6 +49,39 @@ public class Connection_JDBC {
 			}
 		}
 
+	}
+
+
+	private static void writeempData() {
+		Connection conn = getsqlConnection();
+		try {
+
+			if (conn != null) {
+				String sqlInsert = "INSERT INTO employee_payrollday34 (empid ,empname ,empsalary, dateofjoin ,gender) values (?,?,?,?,?)";
+				PreparedStatement connecting = conn.prepareStatement(sqlInsert);
+				connecting.setInt(1, 001);
+				connecting.setString(2, "Shobhit");
+				connecting.setInt(3, 5000);
+				connecting.setString(4, "01-01-2015");
+				connecting.setString(5, "Male");
+
+				int rowInserted = connecting.executeUpdate();
+				if (rowInserted > 0) {
+					System.out.println("data is added !");
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
 	}
 	private static Connection getsqlConnection() {
 		Connection conn = null;
@@ -64,4 +100,3 @@ public class Connection_JDBC {
 
 
 }
-
